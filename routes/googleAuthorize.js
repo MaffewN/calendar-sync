1128fb99df13399c128fd78d2c2d3b86b2e385c8
Celamaf <matthew.d.nicol@gmail.com>
2020-03-26 10:42:41 +0100

    test

--- routes/googleAuthorize.js
+++ routes/googleAuthorize.js
@@ -3,22 +3,8 @@ var router = express.Router();
 
 /* GET /authorize. */
 router.get('/', async function(req, res, next) {
-    // Get auth code
-    const code = req.query.code;
   
-    // If code is present, use it
-    if (code) {
-      try {
-        await authHelper.getTokenFromCode(code, res);
-        // Redirect to home
-        res.redirect('/google-calendar');
-      } catch (error) {
-        res.render('error', { title: 'Error', message: 'Error exchanging code for token', error: error });
-      }
-    } else {
-      // Otherwise complain
-      res.render('error', { title: 'Error', message: 'Authorization error', error: { status: 'Missing code parameter' } });
-    }
+    res.redirect('/google-calendar');
   });
 
 module.exports = router;
